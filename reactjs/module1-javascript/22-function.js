1bef5dbfe1659715089f38231f47466cb1bc7360
Brijesh1990 <bkpandey.pandey@gmail.com>
2025-01-10 10:05:28 +0530

    10-01-2025 all data uploaded

--- reactjs/module1-javascript/22-function.js
+++ reactjs/module1-javascript/22-function.js
@@ -4,7 +4,8 @@ what is function in javascript  ?
 function : A function is group of code that can be completed any task i.e called function.
 or 
 A function is a group of or collection of any code that can be completed any task i.e called function.
-
+or
+A function is a block of code i.e used to complete any task that is called function
 examples :  How to define any function 
 
 function functionname()
@@ -15,7 +16,6 @@ statements;
 }
 
 function call here
-
 
 
 */
@@ -39,12 +39,74 @@ function call here
 
 // display();
 
-function display()
-{
-var a=20;
-var b=30;
-var c=a+b;
-console.log("The additions of numbers is :",c);
-}
+// function display()
+// {
+// var a=20;
+// var b=30;
+// var c=a+b;
+// console.log("The additions of numbers is :",c);
+// }
 
-display();
+// display();
+
+
+// types of function ....
+//    a) user defined function 
+//    b) pre defined function 
+
+// user define function ...
+//    function test()
+//    {
+//     var name="Hi my name is Shvansh";
+//     console.log(name);
+//    }
+
+//    test();
+
+
+// pre defined function
+//  var name="17.56";
+//  console.log(typeof(name));
+// var today=new Date();
+// console.log(today);
+
+
+// how to call function ....
+// two way to call any function ...
+//    a) function call by value 
+//    b) function call by reference 
+
+
+//a) function call by value
+ 
+// function display(a,b) // and b is arguments or parameter of function 
+// {
+//   var c=a+b;
+//   console.log("The additions of numbers is :",c);
+// }
+// display(10,30);
+
+
+//b) function call by reference 
+
+// function display(fname)
+// {
+//     var firstname="The firstname of Users is :"+fname;
+//     console.log(firstname);
+// }
+// display("Vishwa Patel");
+
+
+function calc(a,b)
+{
+    var subs=a-b;
+    var mult=a*b;
+    var add=a+b;
+    var dv=a/b;
+    console.log(subs);
+    console.log(add);
+    console.log(mult);
+    console.log(dv);
+
+}
+calc(20,10);
