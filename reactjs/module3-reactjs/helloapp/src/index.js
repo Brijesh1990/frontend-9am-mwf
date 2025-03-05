d8dc81049acc990235b9e3c44d556061806d3937
Brijesh1990 <bkpandey.pandey@gmail.com>
2025-03-05 09:38:02 +0530

    updated

--- reactjs/module3-reactjs/helloapp/src/index.js
+++ reactjs/module3-reactjs/helloapp/src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// React js 15.0 or 16.0 or 17.0
+// import React from "react";
+// import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root=ReactDOM.createRoot(document.getElementById("demo"));
+// root.render(
+
+//     // fragments are defined in react for access multiple html elements 
+//     // <> </>  or <React.Fragment></React.Fragment>  or <div></div>
+//     // every tags are paired and react js is follow <xhtml> rules 
+
+//     <>
+//         <h1 align='center'>Hi my name is Brijesh</h1>
+//         <hr />
+//         <h2 align='center'>Hi my name is Vinay</h2>
+//         <hr />
+//     </>
+// );
+
+
+//React js 19.0.0
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+const root=createRoot(document.getElementById("demo"));
+// const nm="vinay";
+// let nm="vinay"
+// const a=10;
+// const b=20;
+// const c=a+b;
+
+const a=parseInt("10"); //typeCasting rules
+const b=20;
+const c=a+b;
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <StrictMode>
+     {/* <h1>Hey vinay</h1>
+     <h3>Hey Heet</h3>         */}
+     {/* <h1>Hi my name is :{nm}</h1> */}
+     <h1>Additions of numbers is :{c}</h1>
+    </StrictMode>
+
+)
